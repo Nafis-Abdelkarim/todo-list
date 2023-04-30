23af352915b637a91180e690603130a2637f8fc4
Nafis-Abdelkarim <kazcrb@gmail.com>
2023-04-30 23:45:02 +0100

    Update the task and save it

--- src/Components/EditTodoForm.js
+++ src/Components/EditTodoForm.js
@@ -1,26 +1,18 @@
 import React, {useState} from 'react'
 
 export const EditTodoForm = ({editTodo, task}) => {
-    const [value, setValue] = useState('');
+    const [value, setValue] = useState(task.task);
 
     const handleSubmit = (e) => {
       // prevent default action
         e.preventDefault();
-        if (value) {
-          // Edit todo
-            editTodo(value, task.id);
-          // clear form after submission
-            setValue('');
-        }
+        // edit todo
+        editTodo(value, task.id);
         };
     return (
-        <form onSubmit={handleSubmit} className="TodoForm">
-            
-            <input type="text" value={value} 
-            onChange={(e) => setValue(e.target.value)} className="todo-input" 
-            placeholder='Update your todo' />
-
-            <button type="submit" className='todo-btn'>Update Task</button>
-        </form>
-    )
+    <form onSubmit={handleSubmit} className="TodoForm">
+    <input type="text" value={value} onChange={(e) => setValue(e.target.value)} className="todo-input" placeholder='Update task' />
+    <button type="submit" className='todo-btn'>Add Task</button>
+    </form>
+)
 }
--- src/Components/TodoWrapper.js
+++ src/Components/TodoWrapper.js
@@ -4,48 +4,62 @@ import { TodoForm } from "./TodoForm";
 import { v4 as uuidv4 } from "uuid";
 import { EditTodoForm } from "./EditTodoForm";
 
-    const TodoWrapper = () => {
+const TodoWrapper = () => {
     const [todos, setTodos] = useState([]);
 
     const addTodo = (todo) => {
     setTodos([
         ...todos,
         { id: uuidv4(), task: todo, completed: false, isEditing: false },
-        ]);
+    ]);
     }
 
-    const toggelComplete = id =>{
-        setTodos(todos.map(todo => todo.id === id ? 
-            {...todo, completed: !todo.completed} : todo))
+    const deleteTodo = (id) => setTodos(todos.filter((todo) => todo.id !== id));
+
+    const toggleComplete = (id) => {
+    setTodos(
+        todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+        )
+    );
     }
 
-    const deleteTodo = id =>{
-        setTodos(todos.filter(todo => todo.id === id ?
-            {...todo, isEditing: !todo.isEditing} : todo))
+    const editTodo = (id) => {
+    setTodos(
+        todos.map((todo) =>
+        todo.id === id ? { ...todo, isEditing: !todo.isEditing } : todo
+        )
+    );
     }
 
-    const editTodo = id =>{
-        setTodos(todos.map(todo => todo.id === id ))
-    }
+    const editTask = (task, id) => {
+    setTodos(
+        todos.map((todo) =>
+        todo.id === id ? { ...todo, task, isEditing: !todo.isEditing } : todo
+        )
+    );
+    };
 
     return (
     <div className="TodoWrapper">
         <h1>Get Things Done !</h1>
         <TodoForm addTodo={addTodo} />
       {/* display todos */}
-        {todos.map((todo,index) =>(
+        {todos.map((todo) =>
         todo.isEditing ? (
-        <EditTodoForm/>
+            <EditTodoForm editTodo={editTask} task={todo} />
         ) : (
-            <Todo task={todo} key={index} 
-            toggelComplete={toggelComplete}
+        <Todo
+            key={todo.id}
+            task={todo}
             deleteTodo={deleteTodo}
-            editTodo={editTodo}/>
+            editTodo={editTodo}
+            toggleComplete={toggleComplete}
+        />
         )
-        
-        ))}
+        )}
     </div>
-);
+    );
 };
 
 export default TodoWrapper
